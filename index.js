0e0dd586c23894ce596a240e04060e503d1c69cd
1harshit123 <harshitkumar100436@gmail.com>
2025-11-23 13:56:55 +0530

    backlog commits

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ gsap.from(".nav-wrapper", {
     opacity: 0,
     ease: "power2.out"
 });
+
+
+gsap.registerPlugin(SplitText);
+
+let split = SplitText.create(".video-text", { type: "words, chars" })
+
+// now animate the characters in a staggered fashion
+gsap.from(split.chars, {
+    duration: 1,
+    y: 100,         // animate from 100px below
+    autoAlpha: 0,   // fade in from opacity: 0 and visibility: hidden
+    stagger: 0.05,  // 0.05 seconds between each
+});
